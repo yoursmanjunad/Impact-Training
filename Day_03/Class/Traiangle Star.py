dc5e94e996428a555858e73bef416004e6a574fd
Manjunath Irukulla <89455076+yoursmanjunad@users.noreply.github.com>
2023-12-28 21:39:01 +0530

    Updating Day - 3

---
+++ Day_03/Class/Traiangle Star.py
@@ -0,0 +1,13 @@
+'''
+program to print
+1
+2*3
+4*5*6
+7*8*9*10
+11*12*13*14*15
+'''
+#
+# num = 15
+# for i in range(1,num+1):
+#     for j in range(i,num+1):
+#         print(j)
--- Day_03/Starto5.py
+++
@@ -1,6 +0,0 @@
-num = 25
-for i in range(1, num+1):
-    if(i%5==0):
-        print(i)
-    else:
-        print(i,"*", end=" ")
