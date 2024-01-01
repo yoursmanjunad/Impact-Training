9dcab6afd361504b19e0f02ad6e7d7be15d68f6d
Manjunath Irukulla <89455076+yoursmanjunad@users.noreply.github.com>
2024-01-01 08:03:10 +0530

    Pushing Day 5 - ByteXL Contest Q.

---
+++ Day_06/Own/StrMet.py
@@ -0,0 +1,19 @@
+import string
+
+str = "Manjunath"
+var = string.ascii_letters  # contains all lower and uppercase alphabets
+print(var)
+var2 = string.ascii_lowercase #prints only lowercase
+print(var2)
+var3 = string.ascii_uppercase
+print(var3) # prints only uppercase letters
+var4 = string.digits
+print(var4) # prints digits from 0 to 9
+var5 = string.hexdigits
+print(var5) # prints hexadecimal values
+var6 = string.octdigits
+print(var6) ##prints upto 7
+var8 = string.punctuation
+print(var8) # prints all punctuation symbols
+var9 = string.printable
+print(var9)
